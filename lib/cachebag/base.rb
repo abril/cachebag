c9203c2695200286c440c022e51584573b905730
Luis Cipriani <lfcipriani@gmail.com>
2011-10-07 18:30:18 -0300

    the initial version of the main gem flow

--- lib/cachebag/base.rb
+++ lib/cachebag/base.rb
@@ -13,5 +13,23 @@ module CacheBag
       @store
     end
     
+    def request(method, url, headers = {}, body = nil)
+      raise ArgumentError, "You must pass a block containing the next steps for doing the request." unless block_given?
+      
+      http_date = Time.now.httpdate
+      do_request = false
+      
+      if entry = @store.read(url)
+        last_modified = entry[:headers]["Last-Modified"]
+        etag          = entry[:headers]["ETag"]
+      else
+        do_request = true
+      end
+      
+      response = yield(method, url, headers, body) if do_request
+      #cached_content_unless_stale(url, headers) || cache_content!(method, url, headers, body, &block)
+      
+      #TODO: response without etag and last modified, don't store it
+    end
   end
 end
--- test/cachebag/base_test.rb
+++ test/cachebag/base_test.rb
@@ -2,12 +2,56 @@ require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
 
 describe CacheBag::Base do
   before do
-    #@store = CacheBag::Store.new
+    @cache = CacheBag::Base.new
   end
   
   it "should instantiate with memory store by default" do
-    cache = CacheBag::Base.new
-    
-    cache.store.must_be_kind_of CacheBag::MemoryStore
+    @cache.store.must_be_kind_of CacheBag::MemoryStore
   end
+  
+  it "should be a proxy for any request" do
+    block_called = false
+    
+    response = @cache.request(:get, "http://www.example.com") do |method, url, headers, body|
+      block_called = true
+      # response = sample_http_lib.request(method, url, headers, body)
+      [200, {"Content-Type" => "text/html"}, "<p>awesome example</p>"] # expected response format
+    end
+    
+    response[0].must_equal 200
+    response[1]["Content-Type"].must_equal "text/html"
+    response[2].must_equal "<p>awesome example</p>"
+    block_called.must_equal true
+  end
+  
+  # it "should handle responses with max-age greater than 0" do
+  #   now = Time.now.httpdate
+  #   
+  #   response = @cache.request(:get, "http://www.example.com") do
+  #     headers = {
+  #                 "Cache-Control" => "max-age=600", 
+  #                 "Date"          => now,
+  #                 "Last-Modified" => now, 
+  #                 "ETag"          => md5("<p>awesome example</p>").inspect 
+  #               }
+  #     
+  #     [200, headers, "<p>awesome example</p>"]
+  #   end
+  #   
+  #   response[0].must_equal 200
+  #   response[2].must_equal "<p>awesome example</p>"
+  #   
+  #   block_called = false
+  #   response = @cache.request(:get, "http://www.example.com") do
+  #     block_called = true
+  #     [200, {}, "<p>awesome example</p>"]
+  #   end
+  #   
+  #   block_called.must_equal false
+  #   response[0].must_equal 200
+  #   response[2].must_equal "<p>awesome example</p>"
+  #   Time.parse(response[1]["Date"]).must_be(:>, Time.parse(response[1]["Last-Modified"]))
+  #   Time.parse(response[1]["Date"]).must_be(:<, Time.parse(response[1]["Last-Modified"]) + 600)
+  #   response[1]["Last-Modified"].must_equal now
+  # end
 end
